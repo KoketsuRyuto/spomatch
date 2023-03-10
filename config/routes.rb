5ed3819e34938598dbf1df2eea293c2e49031ff4
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-03-10 06:21:21 +0000

    [add ]spomatch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get '/' => 'users#index'
+    resources :users, only: [:show,:edit,:update]
+    resources :sports, only: [:index,:create,:edit,:update]
+    resources :tags, only: [:index,:create,:edit,:update]
+    resources :posts, only: [:index,:show,:destroy] do
+      resources :post_comments, only: [:index,:destroy]
+    end
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :users, only: [:show,:edit,:update,:confilm,:withdraw]
+    resources :posts do
+      resources :post_comments, only: [:create,:destroy]
+      resource :favorites, only: [:create,:destroy]
+    end
+    resources :groups do
+      resources :group_chats, only: [:show,:create]
+    end
+    get 'join' => 'groups#join'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/public/users_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class Public::UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get public_users_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get public_users_edit_url
+    assert_response :success
+  end
+
+  test "should get confilm" do
+    get public_users_confilm_url
+    assert_response :success
+  end
+end
